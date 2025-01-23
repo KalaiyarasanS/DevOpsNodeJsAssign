eb2b689784161ceabf4bb2232dfd12368943d920
KalaiyarasanS <kalaiyarasansubramanian@gmail.com>
2025-01-23 06:34:54 +0000

    add greek

--- app.js
+++ app.js
@@ -9,4 +9,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
    	 console.log(`Server running at http://localhost:${port}`);
 });
+app.get('/greet', (req, res) => {
+	    res.send('Greetings from Node.js!');
+});
 
